1bf776fc941d73c2170bb7798a096c613eedd197
iovxw <iovxw@outlook.com>
2016-10-17 22:20:01 +0800

    fix Result and Error

--- src/spider.rs
+++ src/spider.rs
@@ -2,6 +2,7 @@ use std::io;
 
 use regex::Regex;
 
+use hyper;
 use hyper::client::Client;
 
 use serde_json;
@@ -12,6 +13,33 @@ use select::predicate::{Predicate, Attr, Class, Name};
 
 const JANDAN_HOME: &'static str = "http://jandan.net/";
 const DUOSHUO_API: &'static str = "http://jandan.duoshuo.com/api/threads/listPosts.json";
+
+#[derive(Debug)]
+pub enum SpiderError {
+    Io(io::Error),
+    Net(hyper::error::Error),
+    Json(serde_json::error::Error),
+}
+
+impl From<io::Error> for SpiderError {
+    fn from(err: io::Error) -> SpiderError {
+        SpiderError::Io(err)
+    }
+}
+
+impl From<hyper::error::Error> for SpiderError {
+    fn from(err: hyper::error::Error) -> SpiderError {
+        SpiderError::Net(err)
+    }
+}
+
+impl From<serde_json::error::Error> for SpiderError {
+    fn from(err: serde_json::error::Error) -> SpiderError {
+        SpiderError::Json(err)
+    }
+}
+
+type SpiderResult<T> = Result<T, SpiderError>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Comment {
@@ -32,17 +60,16 @@ pub struct Pic {
     pub comments: Vec<Comment>,
 }
 
-// TODO: fix Result
-pub fn get_comments(id: &str) -> Vec<Comment> {
+pub fn get_comments(id: &str) -> SpiderResult<Vec<Comment>> {
     let url = format!("{}?thread_key={}", DUOSHUO_API, id);
 
     let client = Client::new();
-    let res = client.get(&url).send().unwrap();
+    let res = try!(client.get(&url).send().map_err(SpiderError::Net));
     if !res.status.is_success() {
         panic!(res.status);
     }
 
-    let data: serde_json::Value = serde_json::from_reader(res).unwrap();
+    let data: serde_json::Value = try!(serde_json::from_reader(res).map_err(SpiderError::Json));
     let all_comments = data.find("parentPosts").unwrap();
 
     data.find("hotPosts").unwrap().as_array().unwrap().iter()
@@ -51,31 +78,29 @@ pub fn get_comments(id: &str) -> Vec<Comment> {
         .map(|result| result.unwrap())
         .map(|comment| {
             let author_info = comment.find("author").unwrap();
-            Comment {
+            Ok(Comment {
                 author: author_info.find("name")
                     .expect("undefined \"name\" in comment").as_str().unwrap().to_string(),
                 likes: comment.find("likes")
                     .expect("undefined \"likes\" in comment").as_u64().unwrap(),
                 text: comment.find("message")
                     .expect("undefined \"message\" in comment").as_str().unwrap().to_string()
-        }})
-        .collect::<Vec<_>>()
+            })})
+        .collect::<SpiderResult<Vec<Comment>>>()
 }
 
-// TODO: fix Result
-pub fn get_list<'a>() -> io::Result<Vec<Pic>> {
+pub fn get_list() -> SpiderResult<Vec<Pic>> {
     let client = Client::new();
 
-    let mut res = client.get(JANDAN_HOME).send().unwrap();
+    let mut res = try!(client.get(JANDAN_HOME).send().map_err(SpiderError::Net));
     if !res.status.is_success() {
         panic!(res.status);
     }
 
-    let document = Document::from_read(&mut res).unwrap();
+    let document = try!(Document::from_read(&mut res).map_err(SpiderError::Io));
 
-    let re = Regex::new(r"^[^\s@]+").unwrap();
-    //let mut result: Vec<Pic> = Vec::new();
-    let result = document.find(Attr("id", "list-pic"))
+
+    document.find(Attr("id", "list-pic"))
         .next()
         .unwrap()
         .find(Class("acv_author").or(Class("acv_comment")))
@@ -83,7 +108,9 @@ pub fn get_list<'a>() -> io::Result<Vec<Pic>> {
         .chunks(2)
         .map (|x| {
             let author_raw = x[0].first_child().unwrap().text();
-            let author = re.captures(&author_raw).unwrap().at(0).unwrap().to_string();
+            let author_filter = Regex::new(r"^[^\s@]+").unwrap();
+            let author = author_filter.captures(&author_raw).unwrap().at(0).unwrap().to_string();
+
             let link = x[0].find(Name("a")).next().unwrap().attr("href").unwrap().to_string();
 
             let id = link.split('#').last().unwrap().to_string();
@@ -107,9 +134,9 @@ pub fn get_list<'a>() -> io::Result<Vec<Pic>> {
             let oo = if let Some(oo) = vote.get(0) { oo.parse::<u32>().unwrap_or(0) } else { 0 };
             let xx = if let Some(xx) = vote.get(1) { xx.parse::<u32>().unwrap_or(0) } else { 0 };
 
-            let comments = get_comments(&id);
+            let comments = try!(get_comments(&id));
 
-            Pic {
+            Ok(Pic {
                 author: author,
                 link: link,
                 id: id,
@@ -118,9 +145,7 @@ pub fn get_list<'a>() -> io::Result<Vec<Pic>> {
                 text: text,
                 images: images,
                 comments: comments,
-            }
+            })
         })
-        .collect();
-
-    Ok(result)
+        .collect::<SpiderResult<Vec<Pic>>>()
 }
